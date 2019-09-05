ab66f9af79e17149a5f2fe24dca52ad42334f20c
Ryan Gallamoza <ryan.gallamoza@gmail.com>
2019-09-04 21:50:31 -0400

    Changed name of window to PoolGame

--- main.cpp
+++ main.cpp
@@ -470,7 +470,7 @@ int main(int argc, char **argv)
 
   // Open a window and create its OpenGL context
 
-  window = glfwCreateWindow(win_w, win_h, "creatures", NULL, NULL);
+  window = glfwCreateWindow(win_w, win_h, "PoolGame", NULL, NULL);
   if( window == NULL ){
     fprintf( stderr, "Failed to open GLFW window. If you have an Intel GPU, they are not 3.3 compatible. Try the 2.1 version of the tutorials.\n" );
     getchar();
